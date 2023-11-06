64f17194cd3ec9ee3678f3e22e1f7fdbc07e79c4
Almaz <almaz.idiyatullin7@gmail.com>
2023-11-06 15:24:12 +0300

    Restructurization of files

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -3,14 +3,15 @@ from typing import Optional, Union
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
 
-from auth.database import User, get_user_db
+from auth.models import User
+from auth.utils import get_user_db
 
-SECRET = "SECRET"
+from config import SECRET_AUTH
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    reset_password_token_secret = SECRET
-    verification_token_secret = SECRET
+    reset_password_token_secret = SECRET_AUTH
+    verification_token_secret = SECRET_AUTH
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
@@ -42,6 +43,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
 
         return created_user
     
+
     async def validate_password(
         self, password: str, user: Union[schemas.UC, models.UP]
     ) -> None:
@@ -67,6 +69,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
             raise exceptions.InvalidPasswordException('Password should have UPPER and LOWER symbols')
         
         return
+
 
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
